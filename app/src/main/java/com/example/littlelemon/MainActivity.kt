264226291fbedd152029ab18d2a2942c66d74793
Stefan Bayne <stefanbusiness95@gmail.com>
2023-06-05 19:51:15 -0400

    finished demo.

---
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -0,0 +1,152 @@
+package com.example.littlelemon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LittleLemonTheme {
+                val databaseMenuItems = database.menuItemDao().getAll().observeAsState(emptyList())
+                val orderMenuItems = remember {
+                    mutableStateOf(false)
+                }
+                var menuItems = if (orderMenuItems.value) {
+                    databaseMenuItems.value.sortedBy { it.title }
+                } else {
+                    databaseMenuItems.value
+                }
+
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "logo",
+                        modifier = Modifier.padding(50.dp)
+                    )
+                    Button(onClick = { orderMenuItems.value = true }) {
+                        Text(text = "Tap to Order By Name")
+                    }
+
+                    val searchPhrase = remember {
+                        mutableStateOf("")
+                    }
+
+                    OutlinedTextField(
+                        value = searchPhrase.value,
+                        onValueChange = { searchPhrase.value = it },
+                        label = { Text("Search") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 50.dp)
+                    )
+
+                    if (searchPhrase.value.isNotEmpty()) {
+                        menuItems = menuItems.filter { it.title.contains(searchPhrase.value) }
+                    }
+                    MenuItemsList(items = menuItems)
+                }
+            }
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            if (database.menuItemDao().isEmpty()) {
+                saveMenuToDatabase(fetchMenu())
+            }
+        }
+    }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+
+        val response = httpClient
+            .get(URL)
+            .body<MenuNetwork>()
+        return response.menu
+    }
+
+    private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
+        val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
+    }
+
+    companion object {
+        const val URL = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json"
+    }
+}
+
+@Composable
+private fun MenuItemsList(items: List<MenuItemRoom>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(top = 20.dp)
+    ) {
+        items(
+            items = items,
+            itemContent = { menuItem ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(menuItem.title)
+                    Text(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(5.dp),
+                        textAlign = TextAlign.Right,
+                        text = "%.2f".format(menuItem.price)
+                    )
+                }
+            }
+        )
+    }
+}
